82233e8aef67787efd9655603cf66d108f0fc92d
snowstorm003 <tusharmukhopadhyay03@gmail.com>
2023-11-16 22:08:51 +0530

    deployed first smart contract

--- Hello-World/hardhat.config.js
+++ Hello-World/hardhat.config.js
@@ -1,6 +1,14 @@
-require("@nomicfoundation/hardhat-toolbox");
-
-/** @type import('hardhat/config').HardhatUserConfig */
+require("dotenv").config();
+require("@nomiclabs/hardhat-ethers");
+const { PRIVATE_KEY } = process.env;
 module.exports = {
-  solidity: "0.8.19",
+  solidity: "0.8.9",
+  defaultNetwork: "sepolia",
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/9gGegDiAUwvATcrPwfvgUx3FeVJ0_1gj",
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
 };
